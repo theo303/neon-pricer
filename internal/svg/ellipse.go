5f2309c3a08fa4d9cad7c2585e7d9ac9d27d557d
theo303 <theo.coutant@join-jump.com>
2023-10-21 19:41:04 +0200

    ellipse parametrization to polar coordinates

--- internal/svg/ellipse.go
+++ internal/svg/ellipse.go
@@ -5,22 +5,46 @@ import (
 	"math"
 )
 
+type arc struct {
+	// center of the ellipse.
+	center point
+	// rx and ry are the radii of the ellipse.
+	rx, ry float64
+	// phi is the angle from the x-axis of the coordonate system to the x-axis of the ellipse.
+	phi float64
+	// angles of the arc.
+	startAngle, endAngle float64
+
+	clockwise bool
+}
+
+// compute angle between two vectors.
+func angle(ux, uy, vx, vy float64) float64 {
+	dot := ux*vx + uy*vy
+	mod := math.Sqrt((ux*ux + uy*uy) * (vx*vx + vy*vy))
+	angle := math.Acos(dot / mod)
+	if ux*vy-uy*vx < 0 {
+		return -angle
+	}
+	return angle
+}
+
 // https://stackoverflow.com/questions/9017100/calculate-center-of-svg-arc
 // https://www.w3.org/TR/SVG/implnote.html#ArcImplementationNotes
-func arcToCenterParam(start, end point, rx, ry, rot float64, fA, fS bool) (point, error) {
+func arcToCenterParam(start, end point, rx, ry, rot float64, fA, fS bool) (arc, error) {
 	if rx == 0 || ry == 0 {
-		return point{}, errors.New("rx or ry cannot be equal to 0")
+		return arc{}, errors.New("rx or ry cannot be equal to 0")
 	}
 	rx = math.Abs(rx)
 	ry = math.Abs(ry)
 
-	phi := rot * math.Pi / 180.0
+	phi := rot * math.Pi / 180
 
 	cosPhi := math.Cos(phi)
 	sinPhi := math.Sin(phi)
 
-	halfDiffX := (start.x - end.x) / 2.0
-	halfDiffY := (start.y - end.y) / 2.0
+	halfDiffX := (start.x - end.x) / 2
+	halfDiffY := (start.y - end.y) / 2
 
 	x1 := cosPhi*halfDiffX + sinPhi*halfDiffY
 	y1 := -sinPhi*halfDiffX + cosPhi*halfDiffY
@@ -42,7 +66,32 @@ func arcToCenterParam(start, end point, rx, ry, rot float64, fA, fS bool) (point
 		y: sinPhi*cx + cosPhi*cy + halfSumY,
 	}
 
-	// x1 :=
+	xcr1 := (x1 - cx) / rx
+	ycr1 := (y1 - cy) / ry
+	startAngle := angle(1, 0, xcr1, ycr1)
 
-	return center, nil
+	xcr2 := (-x1 - cx) / rx
+	ycr2 := (-y1 - cy) / rx
+	endAngle := math.Abs(math.Mod(startAngle+angle(xcr1, ycr1, xcr2, ycr2), math.Pi*2))
+
+	return arc{
+		center:     center,
+		rx:         rx,
+		ry:         ry,
+		phi:        phi,
+		startAngle: startAngle,
+		endAngle:   endAngle,
+		clockwise:  fS,
+	}, nil
+}
+
+func (a arc) point(t float64) point {
+	cosPhi := math.Cos(a.phi)
+	sinPhi := math.Sin(a.phi)
+	rxCosT := a.rx * math.Cos(t)
+	rySinT := a.ry * math.Sin(t)
+	return point{
+		x: cosPhi*rxCosT - sinPhi*rySinT + a.center.x,
+		y: sinPhi*rxCosT + cosPhi*rySinT + a.center.y,
+	}
 }
--- internal/svg/ellipse_test.go
+++ internal/svg/ellipse_test.go
@@ -1,6 +1,7 @@
 package svg
 
 import (
+	"math"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -15,7 +16,7 @@ func Test_arcToCenterParam(t *testing.T) {
 		rot   float64
 		fA    bool
 		fS    bool
-		want  point
+		want  arc
 	}{
 		"1": {
 			start: point{10, 0},
@@ -23,6 +24,29 @@ func Test_arcToCenterParam(t *testing.T) {
 			rx:    10,
 			ry:    10,
 			fA:    true,
+			want: arc{
+				center:     point{0, 0},
+				rx:         10,
+				ry:         10,
+				startAngle: 0,
+				endAngle:   math.Pi / 2,
+			},
+		},
+		"1.5": {
+			start: point{10, 0},
+			end:   point{0, 10},
+			rx:    10,
+			ry:    10,
+			fA:    true,
+			fS:    true,
+			want: arc{
+				center:     point{10, 10},
+				rx:         10,
+				ry:         10,
+				startAngle: -math.Pi / 2,
+				endAngle:   math.Pi,
+				clockwise:  true,
+			},
 		},
 	}
 	for name, tt := range tests {
@@ -30,6 +54,31 @@ func Test_arcToCenterParam(t *testing.T) {
 			got, err := arcToCenterParam(tt.start, tt.end, tt.rx, tt.ry, tt.rot, tt.fA, tt.fS)
 			assert.NoError(t, err)
 			assert.Equal(t, tt.want, got)
+		})
+	}
+}
+
+func Test_arc_point(t *testing.T) {
+	tests := map[string]struct {
+		arc  arc
+		t    float64
+		want point
+	}{
+		"1": {
+			arc: arc{
+				center:     point{0, 0},
+				rx:         10,
+				ry:         10,
+				startAngle: 0,
+				endAngle:   math.Pi / 2,
+			},
+			t:    0,
+			want: point{10, 0},
+		},
+	}
+	for name, tt := range tests {
+		t.Run(name, func(t *testing.T) {
+			assert.Equal(t, tt.want, tt.arc.point(tt.t))
 		})
 	}
 }
